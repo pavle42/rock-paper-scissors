4ec2c8f174d5d97af75d28872458b36da4f37e6a
Pavle <pavlemilovanovic42@gmail.com>
2025-07-06 19:02:12 +0200

    Add a function that plays the game once

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let randomValue = Math.random();
 
@@ -19,5 +22,64 @@ function getHumanChoice() {
     return prompt("Enter your pick: ");
 }
 
-console.log(getHumanChoice());
-console.log(getComputerChoice());
+function playRound(humanChoice, computerChoice) {
+    let lowerHumanChoice = humanChoice.toLowerCase();
+
+    switch (lowerHumanChoice)
+    {
+        case "rock":
+            switch (computerChoice)
+            {
+                case "rock":
+                    console.log(`Tie! Nobody gets a point.`);
+                    break;
+                case "paper":
+                    console.log("You lose. Paper beats Rock.");
+                    computerScore++;
+                    break;
+                case "scissors":
+                    console.log("You win! Rock beats Scissors.");
+                    humanScore++;
+                    break;
+                }
+            break;
+        case "paper":
+            switch (computerChoice)
+            {
+                case "rock":
+                    console.log("You win! Paper beats Rock.");
+                    humanScore++;
+                    break;
+                case "paper":
+                    console.log("Tie! Nobody gets a point.");
+                    break;
+                case "scissors":
+                    console.log("You lose. Scissors beat Paper.");
+                    computerScore++;
+                    break;
+                }
+            break;
+        case "scissors":
+            switch (computerChoice)
+            {
+                case "rock":
+                    console.log("You lose! Rock beats Scissors.");
+                    computerScore++;
+                    break;
+                case "paper":
+                    console.log("You win! Scissors beat Paper.");
+                    humanScore++;
+                    break;
+                case "scissors":
+                    console.log("Tie! Nobody gets a point.");
+                    break;
+                }
+            break;
+        default:
+            break;
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection)
